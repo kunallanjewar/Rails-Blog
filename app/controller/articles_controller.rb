b7678faf4477bebbc48d85c7af3937f0735b1c1b
Kunal Lanjewar <kunallanjewar@gmail.com>
2016-07-27 18:05:55 +0000

    added article listing & removed redundancy

--- app/controller/articles_controller.rb
+++ app/controller/articles_controller.rb
@@ -1,7 +1,25 @@
 class ArticlesController < ApplicationController
+  
+    before_action :set_article, only:[:edit, :update, :show, :destroy]
+    
+    def index
+      @articles = Article.all
+    end
     
     def new
       @article = Article.new
+    end
+    
+    def edit
+    end
+    
+    def update
+      if @article.update(article_params)
+        flash[:notice] = "You have updated your article."
+        redirect_to article_path(@article)
+      else
+        render 'edit'
+      end
     end
     
     def create
@@ -15,7 +33,13 @@ class ArticlesController < ApplicationController
     end 
     
     def show
-      @article = Article.find(params[:id])
+    end
+    
+    def destroy
+      @article.destroy
+      flash[:notice] = "Article was succesfully deleted!"
+      
+      redirect_to articles_path
     end
     
     
@@ -23,6 +47,10 @@ class ArticlesController < ApplicationController
     
     def article_params
       params.require(:article).permit(:title, :description)
+    end
+    
+    def set_article
+    @article = Article.find(params[:id])
     end
     
 end
